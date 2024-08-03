13ebc94873899f0d82e4d1b47ea2c5d571a248e9
caleblandry1 <caleb.landry1@dcmail.ca>
2024-08-02 20:42:09 -0400

    final commit

--- assignment4/part 2/gallery-start/gallery-start/main.js
+++ assignment4/part 2/gallery-start/gallery-start/main.js
@@ -13,7 +13,8 @@ const alts = {
   'pic4.jpg' : 'Wall from an egyption tomb',
   'pic5.jpg' : 'A large moth'
 }
-/* Declaring the alternative text for each image file */
+
+/* Looping through images */
 for (const image of images) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${image}`);
@@ -24,11 +25,17 @@ for (const image of images) {
       displayedImage.alt = e.target.alt;
     });
   }
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
